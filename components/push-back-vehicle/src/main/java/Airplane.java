4ea9afbd8388fe42a437c0dc1dd2ef74c6ec745b
Laura Kaipl <laura.kaipl@online.de>
2018-01-30 15:54:45 +0100

    PushBackVehicle-files added

---
+++ components/push-back-vehicle/src/main/java/Airplane.java
@@ -0,0 +1,3 @@
+//dummy
+
+public class Airplane { }
---
+++ components/push-back-vehicle/src/main/java/Configuration.java
@@ -0,0 +1,13 @@
+//dummy
+
+public enum Configuration {
+
+    instance;
+
+    public String fileSeparator = System.getProperty("file.separator");
+    public String userDirectory = System.getProperty("user.dir");
+
+    public String commonPathToJavaArchive = "abc";
+
+
+}
